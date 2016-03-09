dfbfb9ae4710d407816b533229b322a8398f3f01
anderslemke <mail@anderslemke.dk>
2016-03-09 15:23:44 +0100

    Add a few more ligatures

--- src/ligatures.js
+++ src/ligatures.js
@@ -1,7 +1,11 @@
 module.exports = function(text){
 
-    text = text.replace(/fi/g, 'ﬁ');
-    text = text.replace(/fl/g, 'ﬂ');
+  text = text.replace(/ff/g, 'ﬀ');
+  text = text.replace(/fi/g, 'ﬁ');
+  text = text.replace(/ffi/g, 'ﬃ');
+  text = text.replace(/fl/g, 'ﬂ');
+  text = text.replace(/ffl/g, 'ﬄ');
+  text = text.replace(/ft/g, 'ﬅ');
 
-    return text;
+  return text;
 };
