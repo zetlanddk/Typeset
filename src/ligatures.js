f90cf256c40b1b45e7173cbe86fbf254fdb943dc
anderslemke <mail@anderslemke.dk>
2016-03-09 15:37:37 +0100

    Use html entity for ft

--- src/ligatures.js
+++ src/ligatures.js
@@ -1,11 +1,11 @@
 module.exports = function(text){
 
+  text = text.replace(/ffi/g, 'ﬃ');
+  text = text.replace(/ffl/g, 'ﬄ');
   text = text.replace(/ff/g, 'ﬀ');
   text = text.replace(/fi/g, 'ﬁ');
-  text = text.replace(/ffi/g, 'ﬃ');
   text = text.replace(/fl/g, 'ﬂ');
-  text = text.replace(/ffl/g, 'ﬄ');
-  text = text.replace(/ft/g, 'ﬅ');
+  text = text.replace(/ft/g, '&#64261;');
 
   return text;
 };
