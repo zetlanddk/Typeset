2bfeb99fd3c101d08e38f74f67a900e7fd6b151e
anderslemke <mail@anderslemke.dk>
2016-03-29 13:46:25 +0200

    Only use fi and fl

--- src/ligatures.js
+++ src/ligatures.js
@@ -1,8 +1,5 @@
 module.exports = function(text){
 
-  text = text.replace(/ffi/g, 'ﬃ');
-  text = text.replace(/ffl/g, 'ﬄ');
-  text = text.replace(/ff/g, 'ﬀ');
   text = text.replace(/fi/g, 'ﬁ');
   text = text.replace(/fl/g, 'ﬂ');
 
